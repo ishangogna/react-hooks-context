c848077341d7c0c067951a88d1b7eb0141cfdd77
Ishan Gogna <ishangogna10@gmail.com>
2020-07-17 20:53:43 +0530

    creating/consuming multiple contexts

--- hooks-context-app/src/App.js
+++ hooks-context-app/src/App.js
@@ -3,14 +3,17 @@ import Navbar from './components/Navbar';
 import BookList from './components/Booklist';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ToggleTheme from './components/toggleTheme';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ToggleTheme />
+      <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ToggleTheme />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
--- hooks-context-app/src/components/Booklist.js
+++ hooks-context-app/src/components/Booklist.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 class BookList extends Component {
     state = {
         books : [
@@ -10,20 +11,33 @@ class BookList extends Component {
     }
     render() { 
         return(
-            <ThemeContext.Consumer>{(context)=>{
-                const { isLightTheme, light, dark } = context;
-                const theme = isLightTheme ? light : dark;
-                return(
-                    <div className = 'book-list' style = {{background : theme.ui, color : theme.syntax}}>
-                        <ul>
-                            {this.state.books.map(book => (
-                                <li key = {book.id}>{book.title}</li>
-                            ))}
-                        </ul>
-                    </div>
-                )
-            }}
-            </ThemeContext.Consumer>
+            <AuthContext.Consumer>{(authContext)=> (
+                <ThemeContext.Consumer>{(context)=>{
+                    const { isAuthenticated,authenticate } = authContext;
+                    const { isLightTheme, light, dark } = context;
+                    const theme = isLightTheme ? light : dark;
+                    return(
+                        <div>
+                            <div className = 'book-list' style = {{background : theme.ui, color : theme.syntax}}>
+                                <ul>
+                                    {this.state.books.map(book => (
+                                        <li key = {book.id}>{book.title}</li>
+                                    ))}
+                                </ul>
+                                
+                            </div>
+                            <div onClick = {authenticate} style = {{cursor : 'pointer'}}>
+                                    { 
+                                        isAuthenticated ? 'Logged in' : 'Logged out'
+                                    }
+                            </div>
+                        </div>
+                        )
+                    }}
+                </ThemeContext.Consumer>
+            )}
+            
+        </AuthContext.Consumer>
             
         )
     }    
